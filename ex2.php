404295fd5a95765767b650287bd52062f160c600
Shruti Vijay Chougale <157224983+shrutiscoding@users.noreply.github.com>
2024-03-24 08:55:45 +0530

    Add files via upload

---
+++ ex2.php
@@ -0,0 +1,15 @@
+<?php
+    $num=-1;
+	if($num>0)
+	{
+	  echo "$num is positive number !!";
+	}
+	elseif($num==0)
+	{
+	 echo "$num is neither positive nor negative number !!";
+	}
+	else
+	{
+	  echo "$num is negative number !!";
+	}
+	?>
---
+++ ex3.php
@@ -0,0 +1,34 @@
+<?php
+$month=7;
+echo "You have entered";
+switch($month)
+{
+ case 1: echo" January Month !!";
+ break;
+ case 2: echo" February Month !!";
+ break;
+ case 3: echo" March Month !!";
+ break;
+ case 4: echo" April Month !!";
+ break;
+ case 5: echo" May Month !!";
+ break;
+ case 6: echo" June Month !!";
+ break;
+ case 7: echo" July Month !!";
+ break;
+ case 8: echo" August Month !!";
+ break;
+ case 9: echo" September Month !!";
+ break;
+ case 10: echo" October Month !!";
+ break;
+ case 11: echo" November Month !!";
+ break;
+ case 12: echo" December Month !!";
+ break;
+ default: echo "Enter correct month!!";
+ break;
+} 
+echo"<br> Exited";
+?>
